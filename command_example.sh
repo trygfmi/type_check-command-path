67b9ec7464c4e6b06bc67b611885b112b5b04329
terner-running-terminal-commands <runningterminalcommands@gmail.com>
2025-12-29 19:34:55 +0900

    first dev commit

---
+++ command_example.sh
@@ -0,0 +1,14 @@
+#"!/bin/bash
+# ./command_example.sh
+
+
+source ~/bashrc_folder/macports_alias
+source ./alias_file
+type git
+
+type cat
+source ./alias_file
+type cat
+# ターミナルを閉じれば元に戻る
+# whichは絶対パスだけ、typeはaliasも反映される
+
